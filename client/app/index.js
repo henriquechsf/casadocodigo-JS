15c037373e2e166336be5b06ea2ae3d562f48176
Carlos Henrique <ch.ferreir@hotmail.com>
2020-02-27 11:48:58 -0300

    pag 11 - criando td com o evento submit

---
+++ client/app/index.js
@@ -0,0 +1,42 @@
+var campos = [
+	document.querySelector('#data'),
+	document.querySelector('#quantidade'),
+	document.querySelector('#valor')
+];
+console.log(campos);
+
+// tbody - recebera a tr que vamos construir
+var tbody = document.querySelector('table tbody');
+
+document.querySelector('.form').addEventListener('submit', function(event) {
+	// cancelando a submissão do formulário
+	event.preventDefault();
+
+	// cria uma nova linha na tabela
+	var tr = document.createElement('tr');
+
+	campos.forEach(function(campo) {
+		// cria um td sem informações
+		var td = document.createElement('td');
+
+		// atribui o valor do campo à td
+		td.textContent = campo.value;
+
+		// adiciona o td na tr
+		tr.appendChild(td);
+	})
+
+	// nova td que armazena o volume da negociação
+	var tdVolume = document.createElement('td');
+	tdVolume.textContent = campos[1].value * campos[2].value;
+
+	tr.appendChild(tdVolume);
+	tbody.appendChild(tr);
+
+	// limpa os campos do formulário
+	campos[0].value = '';
+	campos[1].value = '';
+	campos[2].value = '';
+	campos[0].focus();
+
+})
