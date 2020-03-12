a5fe575ef308a06f4b5cc2c0bd7b87b8c348a2b0
Carlos Henrique <ch.ferreir@hotmail.com>
2020-03-12 12:03:43 -0300

    pg45 - Encapsulamento da classe Negociação garantindo imutabilidade dos dados

--- client/app/domain/negociacao/Negociacao.js
+++ client/app/domain/negociacao/Negociacao.js
@@ -1,11 +1,20 @@
 class Negociacao {
     constructor(data, quantidade, valor) {
+        /*
         this._data = new Date(data.getTime())
         this._quantidade = quantidade
         this._valor = valor
 
         // congela o objeto para não alterar os atributos
         Object.freeze(this)
+        */
+       // forma de encapsulamento com Object.assign()
+       Object.assign(this, {
+           _data: new Date(data.getTime()),
+           _quantidade: quantidade,
+           _valor: valor
+       });
+       Object.freeze(this);
     }
 
     get volume() {
