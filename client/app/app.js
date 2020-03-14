8bd9a20d1170e6e03ffab4bc12be02c41d2a5e24
Carlos Henrique <ch.ferreir@hotmail.com>
2020-03-13 21:02:19 -0300

    Evitando percorrer a DOM várias vezes

--- client/app/app.js
+++ client/app/app.js
@@ -1,5 +1,8 @@
 // instancia do controller
 let controller = new NegociacaoController()
 
+// bind aqui
 // associa o evento do formulario ao metodo adiciona do controller
-document.querySelector('form').addEventListener('submit', controller.adiciona)
+document
+    .querySelector('form')
+    .addEventListener('submit', controller.adiciona.bind(controller))
--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -1,19 +1,22 @@
 class NegociacaoController {
 
-    adiciona(event) {
-        event.preventDefault()
-
+    constructor() {
         // atribuindo uma variavel $ ao querySelector
         // realizando bind, $ mantém document como o seu contexto this
         let $ = document.querySelector.bind(document)
-        
+
         // buscando os elementos
-       let inputData = $('#data')
-       let inputQuantidade = $('#quantidade')
-       let inputValor = $('#valor')
-       
-       console.log(inputData.value)
-       console.log(parseInt(inputQuantidade.value))
-       console.log(parseFloat(inputValor.value))
+        this._inputData = $('#data')
+        this._inputQuantidade = $('#quantidade')
+        this._inputValor = $('#valor')
+    }
+
+    adiciona(event) {
+        event.preventDefault()
+
+        // precisamos acessar as funcionalidades através do this
+        console.log(this._inputData.value)
+        console.log(parseInt(this._inputQuantidade.value))
+        console.log(parseFloat(this._inputValor.value))
     }
 }
