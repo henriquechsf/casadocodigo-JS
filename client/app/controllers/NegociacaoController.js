e8deab0c5264a5fa9dacf32ab2163517f57de849
Carlos Henrique <ch.ferreir@hotmail.com>
2020-03-14 13:22:20 -0300

    adicionando o modelo Negociacoes.js

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -9,25 +9,39 @@ class NegociacaoController {
         this._inputData = $('#data')
         this._inputQuantidade = $('#quantidade')
         this._inputValor = $('#valor')
+        this._negociacoes = new Negociacoes()
     }
 
     adiciona(event) {
         event.preventDefault();
 
+        // inclui a negociação
+        this._negociacoes.adiciona(this._criaNegociacao())
+
+        // imprime a lista com o novo elemento
+        console.log(this._negociacoes.paraArray())
+
+        // limpando o formulario
+        this._limpaFormulario();
+    }
+
+    _limpaFormulario() {
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+        this._inputData.focus();
+    }
+
+    _criaNegociacao() {
+        // retorna uma instancia da negociação
+
         // instanciando uma nova Negociação
         // passando os dados dos campos do formulario
         // usando metodo estatico para conversao de data
-        let negociacao = new Negociacao(
+        return new Negociacao(
             DateConverter.paraData(this._inputData.value),
             parseInt(this._inputQuantidade.value),
             parseFloat(this._inputValor.value)
-        );
-
-        console.log(negociacao.data)
-
-        // converte data para texto formato dia/mes/ano
-        let diaMesAno = DateConverter.paraTexto(negociacao.data)
-
-        console.log(diaMesAno);
+        )
     }
 }
